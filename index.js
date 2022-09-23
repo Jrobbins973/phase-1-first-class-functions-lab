ba7b398a8a9f89cde5d4fa29b245015e01a726fd
Joe Robbins <joe.robbins.cpt@gmail.com>
2022-09-23 11:59:36 -0400

    commit tree ae080523bc058ef44cab7eefafe7dd8461af0336

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (array) => {
+    let firstTwo = array.slice(0, 2)
+    return firstTwo
+
+    }
+const returnLastTwoDrivers = (array) => {
+    return array.slice(array.length - 2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// const fareQuintupler = (number) => {
+//     return number * 4
+// }
+
+
+const createFareMultiplier = () => {
+   return function fareQuintupler(number) {
+    return number * 5
+   }
+}
+    
+
+const fareDoubler= (fare)=> {
+    return fare * 2
+}
+
+
+
+const fareTripler = fare => {
+    return fare * 3
+}
+
+function selectDifferentDrivers (drivers, selectDrivers ) {
+    return selectDrivers ( drivers )
+}
